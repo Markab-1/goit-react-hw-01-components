ed9943836d7637f4be7e8274570b24f784f6d84b
Markab-1 <velichko.anna.b@gmail.com>
2022-05-07 09:07:57 +0300

    iter-3
    Удалила закомментированную часть

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -45,21 +45,3 @@ export const App = () => {
   </Container>
   );
 }
-
-
-/*export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};*/
